6b6169ccf6ff197dc83f66a57b89a7e47da9871f
wobble.house <ross@wobble.house>
2022-10-03 18:35:08 -0400

    updating homepage & title

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Index({ allPosts: { edges }, preview }) {
   return (
     <Layout preview={preview}>
       <Head>
-        <title>Next.js Blog Example with {CMS_NAME}</title>
+        <title>A2IM Artist - Home</title>
       </Head>
       <Container>
         <Intro />
