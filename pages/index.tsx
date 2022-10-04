d98d2dc26c9bbb59f42789fd4efd0cde2b7ed7a4
wobble.house <ross@wobble.house>
2022-10-03 20:07:27 -0400

    updated index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,39 @@
+import React, { useRef, useState } from "react";
 import Head from 'next/head'
 import { GetStaticProps } from 'next'
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
+import { motion } from "framer-motion"
 import { getAllPostsForHome } from '../lib/api'
-import { CMS_NAME } from '../lib/constants'
+import Intro from '../components/intro'
+import { Hidden } from '@mui/material'
 
-export default function Index({ allPosts: { edges }, preview }) {
-  const heroPost = edges[0]?.node
-  const morePosts = edges.slice(1)
+// adding animation to page transitions
+const artistcontent = (isFirstMount) => ({
+  animate: {
+    transition: { staggerChildren: 0.1, delayChildren: isFirstMount ? 2.8 : 0 },
+  },
+});
+
+export default function Index({ isFirstMount, allPosts: { edges }}) {
+  
+  const morePosts = edges.slice(0,3)
 
   return (
-    <Layout preview={preview}>
-      <Head>
-        <title>A2IM Artist - Home</title>
-      </Head>
-      <Container>
-        <Intro />
-        {heroPost && (
-          <HeroPost
-            title={heroPost.title}
-            coverImage={heroPost.featuredImage}
-            date={heroPost.date}
-            author={heroPost.author}
-            slug={heroPost.slug}
-            excerpt={heroPost.excerpt}
-          />
-        )}
-        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-      </Container>
-    </Layout>
-  )
-}
+      <motion.section exit={{ opacity: 0 }}>
+          <Head>
+              <title>A2IM Artist - Home</title>
+          </Head>
+          <motion.div
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          variants={artistcontent(isFirstMount)}
+          className="">
+          <Intro />
+        </motion.div>
+      </motion.section>
+  );
+  }
+
 
 export const getStaticProps: GetStaticProps = async ({ preview = false }) => {
   const allPosts = await getAllPostsForHome(preview)
@@ -42,4 +42,4 @@ export const getStaticProps: GetStaticProps = async ({ preview = false }) => {
     props: { allPosts, preview },
     revalidate: 10,
   }
-}
+};
